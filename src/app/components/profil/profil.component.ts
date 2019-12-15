6314527ecd3a5f245e21d1dc34d777013c32abc1
cdhbze93 <antoebt@hotmail.com>
2019-12-15 20:36:43 +0100

    style: arrow toggle

--- src/app/components/profil/profil.component.ts
+++ src/app/components/profil/profil.component.ts
@@ -7,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
   // Variables
-
+  public left = false;
+  public rigth = false;
   public languages = ['HTML', 'CSS', 'JavaScript' , 'NodeJS', 'PHP', 'Python', 'C', 'Angular', 'SQL'];
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleSideLeft() {
+    this.left = !this.left;
+  }
+
+  toggleSideRight() {
+    this.rigth = !this.rigth;
+  }
 }
