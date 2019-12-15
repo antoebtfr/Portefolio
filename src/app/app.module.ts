ed839bfc35baf20bbcc0083faaedac8fcab89dde
cdhbze93 <antoebt@hotmail.com>
2019-12-15 11:57:47 +0100

    feat: language / profil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfilComponent } from './components/profil/profil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    ProfilComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/profil/profil.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profil',
+  templateUrl: './profil.component.html',
+  styleUrls: ['./profil.component.scss']
+})
+export class ProfilComponent implements OnInit {
+  // Variables
+
+  public languages = ['HTML', 'CSS', 'JavaScript' , 'NodeJS', 'PHP', 'C', 'Angular'];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
