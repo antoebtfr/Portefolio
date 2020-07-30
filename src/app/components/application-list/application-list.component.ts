c00ce0237632703683f1aed8c11239a9ef0dc721
antoebtfr <antoebt@hotmail.com>
2020-07-30 18:36:59 +0200

    feat: filter name projects

--- src/app/components/application-list/application-list.component.ts
+++ src/app/components/application-list/application-list.component.ts
@@ -7,17 +7,42 @@ import { Application } from 'src/app/shared/class/application';
   styleUrls: ['./application-list.component.scss']
 })
 export class ApplicationListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  public arrayApplications: Application[] = [
-    { name: 'HalloFighters', img: 'https://camo.githubusercontent.com/3387f76995ab5585cdd267729cc2be64235bfa1d/68747470733a2f2f33353361323363353030646465336232616435382d63343966653765373335356433383438343532373066346137613061376161312e73736c2e6366322e7261636b63646e2e636f6d2f3565343363643632343566623632303030383964353836632f73637265656e73686f742e706e67', link: 'https://cranky-shirley-edcd00.netlify.com/', usedTechnos: ['HTML', 'CSS', 'TypeScript'] },
-    { name: 'Space', img: 'https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5e45260c84ed9d0008ec0503/screenshot.png', link: 'https://festive-darwin-3e8cdb.netlify.com/', usedTechnos: ['HTML', 'CSS', 'TypeScript'] },
-    { name: 'Chrono_VanillaJS', img: '../../assets/img/Chrono_Angular.png', link: 'https://sad-nobel-a89988.netlify.app/', usedTechnos: ['HTML', 'CSS', 'JavaScript']}
+private arrayApplications: Application[] = [
+    {
+      name: 'HalloFighters',
+      img:
+        'https://camo.githubusercontent.com/3387f76995ab5585cdd267729cc2be64235bfa1d/68747470733a2f2f33353361323363353030646465336232616435382d63343966653765373335356433383438343532373066346137613061376161312e73736c2e6366322e7261636b63646e2e636f6d2f3565343363643632343566623632303030383964353836632f73637265656e73686f742e706e67',
+      link: 'https://cranky-shirley-edcd00.netlify.com/',
+      usedTechnos: ['HTML', 'CSS', 'TypeScript']
+    },
+    {
+      name: 'Space',
+      img:
+        'https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5e45260c84ed9d0008ec0503/screenshot.png',
+      link: 'https://festive-darwin-3e8cdb.netlify.com/',
+      usedTechnos: ['HTML', 'CSS', 'TypeScript']
+    },
+    {
+      name: 'Chrono_VanillaJS',
+      img: '../../assets/img/Chrono_Angular.png',
+      link: 'https://sad-nobel-a89988.netlify.app/',
+      usedTechnos: ['HTML', 'CSS', 'JavaScript']
+    }
   ];
 
+  public showedArray;
 
-  ngOnInit() {
+  private searchByNameChange() {
+    const searchInput = document.getElementById('search-by-name');
+    searchInput.oninput = () => {
+      this.showedArray = this.arrayApplications.filter(x => x.name.includes(searchInput.value));
+    };
   }
 
+ngOnInit() {
+    this.searchByNameChange();
+    this.showedArray = this.arrayApplications;
+  }
 }
