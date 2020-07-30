d0a6b3911cc09065687aae1592f0b5057a06ea88
antoebtfr <antoebt@hotmail.com>
2020-07-30 19:30:35 +0200

    feat: project searchByTechnos

--- src/app/components/application-list/application-list.component.ts
+++ src/app/components/application-list/application-list.component.ts
@@ -9,20 +9,20 @@ import { Application } from 'src/app/shared/class/application';
 export class ApplicationListComponent implements OnInit {
   constructor() {}
 
-private arrayApplications: Application[] = [
+  private arrayApplications: Application[] = [
     {
       name: 'HalloFighters',
       img:
         'https://camo.githubusercontent.com/3387f76995ab5585cdd267729cc2be64235bfa1d/68747470733a2f2f33353361323363353030646465336232616435382d63343966653765373335356433383438343532373066346137613061376161312e73736c2e6366322e7261636b63646e2e636f6d2f3565343363643632343566623632303030383964353836632f73637265656e73686f742e706e67',
       link: 'https://cranky-shirley-edcd00.netlify.com/',
-      usedTechnos: ['HTML', 'CSS', 'TypeScript']
+      usedTechnos: ['HTML', 'CSS', 'TypeScript', 'Angular']
     },
     {
       name: 'Space',
       img:
         'https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5e45260c84ed9d0008ec0503/screenshot.png',
       link: 'https://festive-darwin-3e8cdb.netlify.com/',
-      usedTechnos: ['HTML', 'CSS', 'TypeScript']
+      usedTechnos: ['HTML', 'CSS', 'TypeScript', 'Angular']
     },
     {
       name: 'Chrono_VanillaJS',
@@ -37,12 +37,30 @@ private arrayApplications: Application[] = [
   private searchByNameChange() {
     const searchInput = document.getElementById('search-by-name');
     searchInput.oninput = () => {
-      this.showedArray = this.arrayApplications.filter(x => x.name.includes(searchInput.value));
+      this.showedArray = this.arrayApplications.filter(x =>
+        x.name.includes(searchInput.value)
+      );
     };
   }
 
-ngOnInit() {
+  private searchByTechno() {
+    const techSelect = document.getElementById('search-by-techno');
+    techSelect.addEventListener('change', () => {
+      if (techSelect.value === 'Toutes') {
+        this.showedArray = this.arrayApplications;
+      } else {
+        this.showedArray = this.arrayApplications.filter(x =>
+          x.usedTechnos.includes(techSelect.value)
+        );
+      }
+    });
+  }
+
+  ngOnInit() {
     this.searchByNameChange();
-    this.showedArray = this.arrayApplications;
+    this.searchByTechno();
+    this.showedArray = this.arrayApplications.sort((a, b) =>
+      a.name > b.name ? 1 : -1
+    );
   }
 }
