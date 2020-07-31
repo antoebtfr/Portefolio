e3b9ee0f76863d136330ed4e48af12d722d3635b
antoebtfr <antoebt@hotmail.com>
2020-07-31 14:20:23 +0200

    archi: reaffectation of 'projects' data

--- src/app/components/application-list/application-list.component.ts
+++ src/app/components/application-list/application-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Application } from 'src/app/shared/class/application';
+import { projects } from '../../../assets/data/project';
 
 @Component({
   selector: 'app-application-list',
@@ -9,34 +10,10 @@ import { Application } from 'src/app/shared/class/application';
 export class ApplicationListComponent implements OnInit {
   constructor() {}
 
-  private arrayApplications: Application[] = [
-    {
-      name: 'HalloFighters',
-      img:
-        'https://camo.githubusercontent.com/3387f76995ab5585cdd267729cc2be64235bfa1d/68747470733a2f2f33353361323363353030646465336232616435382d63343966653765373335356433383438343532373066346137613061376161312e73736c2e6366322e7261636b63646e2e636f6d2f3565343363643632343566623632303030383964353836632f73637265656e73686f742e706e67',
-      link: 'https://cranky-shirley-edcd00.netlify.com/',
-      usedTechnos: ['HTML', 'CSS', 'TypeScript', 'Angular']
-    },
-    {
-      name: 'Space',
-      img:
-        'https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5e45260c84ed9d0008ec0503/screenshot.png',
-      link: 'https://festive-darwin-3e8cdb.netlify.com/',
-      usedTechnos: ['HTML', 'CSS', 'TypeScript', 'Angular']
-    },
-    {
-      name: 'Chrono_VanillaJS',
-      img: '../../assets/img/Chrono_Angular.png',
-      link: 'https://sad-nobel-a89988.netlify.app/',
-      usedTechnos: ['HTML', 'CSS', 'JavaScript']
-    }
-  ];
-
+  private arrayApplications: Application[];
   public showedArray: Application[];
 
-  private search() {
-    /* needfix */
-
+  private search(): void {
 
     const filter = () => {
       if (techno === 'Toutes') {
@@ -71,9 +48,10 @@ export class ApplicationListComponent implements OnInit {
 
 
   ngOnInit() {
-    this.search();
+    this.arrayApplications = projects;
     this.showedArray = this.arrayApplications.sort((a, b) =>
       a.name > b.name ? 1 : -1
     );
+    this.search();
   }
 }
